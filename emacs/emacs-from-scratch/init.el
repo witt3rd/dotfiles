505fc6a0ca279cd9295280bb143c2f005b64e8ff
witt3rd <donald@witt3rd.com>
2021-09-23 17:12:38 -0700

    [emacs] helpful mode

--- emacs/emacs-from-scratch/init.el
+++ emacs/emacs-from-scratch/init.el
@@ -321,6 +321,38 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :bind
+  ([remap describe-function] . helpful-callable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Note that the built-in `describe-function' includes both functions
+;; and macros. `helpful-function' is functions only, so we provide
+;; `helpful-callable' as a drop-in replacement.
+;;(global-set-key (kbd "C-h f") #'helpful-callable)
+;;(global-set-key (kbd "C-h v") #'helpful-variable)
+;;(global-set-key (kbd "C-h k") #'helpful-key)
+
+;; Lookup the current symbol at point. C-c C-d is a common keybinding
+;; for this in lisp modes.
+(global-set-key (kbd "C-c C-d") #'helpful-at-point)
+
+;; Look up *F*unctions (excludes macros).
+;;
+;; By default, C-h F is bound to `Info-goto-emacs-command-node'. Helpful
+;; already links to the manual, if a function is referenced there.
+(global-set-key (kbd "C-h F") #'helpful-function)
+
+;; Look up *C*ommands.
+;;
+;; By default, C-h C is bound to describe `describe-coding-system'. I
+;; don't find this very useful, but it's frequently useful to only
+;; look at interactive functions.
+(global-set-key (kbd "C-h C") #'helpful-command)
+
 ;; -- Emacs --
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
