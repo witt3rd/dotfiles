9a6988a3b6670f6b73b08f4ffd2a23b4fb7911d8
witt3rd <donald@witt3rd.com>
2021-09-26 09:20:16 -0700

    [emacs] update literate config

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,11 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Donald Thompson"
       user-mail-address "donald@witt3rd.com")
 
