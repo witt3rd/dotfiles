be4e1dce2f602b4a3139786b24d3f12d4e067886
witt3rd <donald@witt3rd.com>
2021-09-28 06:43:28 -0700

    [linux] setup updates

--- linux/apt-setup.sh
+++ linux/apt-setup.sh
@@ -5,3 +5,7 @@ sudo apt upgrade -y
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 sudo apt install -y gcc-11 g++-11
+
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add - # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
+
+
