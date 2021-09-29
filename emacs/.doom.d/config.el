6d82d32338c2dcee1da3b2668f3d4af224b16224
witt3rd <donald@witt3rd.com>
2021-09-29 04:18:19 -0700

    [emacs] tidy conf org

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,33 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
 (setq user-full-name "Donald Thompson"
       user-mail-address "donald@witt3rd.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq
  doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 16 :weight 'semi-light)
  doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 16)
  doom-big-font (font-spec :family "FiraCode Nerd Font" :size 36 :weight 'bold))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq custom-safe-themes t)
 (if (window-system)
     (setq doom-theme 'doom-outrun-electric)
   (setq doom-theme 'doom-one))
 (setq fancy-splash-image "~/.doom.d/doom.png")
 
-; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 (setq custom-file null-device)
