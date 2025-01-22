50049df55f1a1e653102a2a6c8f6590c11c20058
Donald Thompson <witt3rd@witt3rd.com>
2025-01-22 06:41:14 -0800

    Update .emacs.custom.el

--- dot_emacs.custom.el
+++ dot_emacs.custom.el
@@ -6,6 +6,7 @@
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+ '(inhibit-startup-screen t)
  '(package-selected-packages '(gruber-darker-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
